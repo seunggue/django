5cb73921246137481d775c7915fe3d28a44eca40
SeungGue <seungue1687@gamil.com>
2019-08-08 11:38:55 +0900

    create완성

--- 05_qna/articles/models.py
+++ 05_qna/articles/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Question(models, Model):
+class Question(models.Model):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=100)
     user = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
-    #auto_now_add=True: 작성일자 표시 안해도 자동으로 현재시간을 입력해줌
+    #auto_now_add=True: 작성일자 표시 안해도 자동으로 서버시간(현재시간)을 입력해줌
--- 05_qna/articles/views.py
+++ 05_qna/articles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from.models import Question
 
 # Create your views here.
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    user = request.GET.get('user')
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    question = Question()
+    question.title = title
+    question.user = user
+    question.content = content
+    question.save()
+
+    return redirect('/questions/')
