92a9803f8889f62e053acc5839033d50c69efd66
seunggue <seungue1687@gmail.com>
2019-10-04 11:12:31 +0900

    create완성

--- board/todos/views.py
+++ board/todos/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,
+    }
+    return render(request, 'index.html', context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    author = request.POST.get('author')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    due_date = request.POST.get('due-date')
+
+    todo = Todo.objects.create(author=author, title=title, content=content, due_date=due_date)
+
+    return redirect('/todos/')
