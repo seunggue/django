6e88937cb7f0c52eab6bed164042565bc2d6198a
seunggue <seungue1687@gmail.com>
2019-10-07 11:09:56 +0900

    detail페이지 완성

--- Asked/questions/views.py
+++ Asked/questions/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    questions = Question.objects.all()
+    context = {
+        'questions': questions
+    }
+
+    return render(request, 'index.html', context)
+
+def detail(request, id):
+    question = Question.objects.get(id=id)
+    context = {
+        'question': question,
+    }
+
+    return render(request, 'detail.html', context)
+
+def create(request):
+    if request.method == "POST":
+        content = request.POST.get('content')
+        Question.objects.create(content = content)
+
+        return redirect('questions:index')
+    else:
+       return render(request, 'form.html')
+
+        
