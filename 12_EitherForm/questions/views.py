a4ede2dfccddf65e2029a072ae433772cee3debf
seunggue <seungue1687@gmail.com>
2019-10-16 09:47:03 +0900

    form.py

--- 12_EitherForm/questions/views.py
+++ 12_EitherForm/questions/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'questions/index.html')
+
+def create(request):
+    if request.method == "POST":
+        pass
+
+    else:
+
+        return render(request, 'questions/form.html')
