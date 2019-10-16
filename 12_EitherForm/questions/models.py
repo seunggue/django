0000448abb0844919ebb9c82a5dea67991f6852f
seunggue <seungue1687@gmail.com>
2019-10-16 09:35:53 +0900

    Questions모델 완성

--- 12_EitherForm/questions/models.py
+++ 12_EitherForm/questions/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Questions(models.Model):
+    title = models.CharField(max_length=100)
+    choice_1 = models.CharField(max_length=100)
+    choice_2 = models.CharField(max_length=100)
