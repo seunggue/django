81782f846a0c4f7d20d00927af74f6b1a401beae
seunggue <seungue1687@gmail.com>
2019-10-16 15:35:17 +0900

    login

--- 12_EitherForm/accounts/views.py
+++ 12_EitherForm/accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+                                # 함수 이름을 login을 auth_login으로 바꿔줌
 
 # Create your views here.
 
@@ -14,5 +16,18 @@ def signup(request):
 
     context = {
         'form':form,
+    }
+    return render(request, 'accounts/form.html', context)
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('questions:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
     }
     return render(request, 'accounts/form.html', context)
