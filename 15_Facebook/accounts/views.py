fa1559d8c2869208b823feace53ad21b9485b511
seunggue <seungue1687@gmail.com>
2019-10-22 09:44:48 +0900

    login완료

---
+++ 15_Facebook/accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+
+    else:
+        form = UserCreationForm()
+        context = {
+            'form':form
+        }
+
+        return render(request, 'accounts/form.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'accounts/form.html', context)
--- 15_Facebook/facebook/urls.py
+++ 15_Facebook/facebook/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
 
-    path('posts/', include('posts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('posts/', include('posts.urls')),
 ]
