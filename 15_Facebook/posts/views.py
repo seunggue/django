27d8aeafdb893e8d429aa5afd0899f5b768a0372
seunggue <seungue1687@gmail.com>
2019-10-22 10:33:50 +0900

    login후 게시글 작성 완료

--- 15_Facebook/posts/views.py
+++ 15_Facebook/posts/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('posts:index')
+
+    else:
+        forms = PostForm()
+    context = {
+        'forms':forms
+    }
+    return render(request, 'posts/form.html', context)
